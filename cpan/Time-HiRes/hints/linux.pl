33a72de6653bd3a257c84bdb8b43a2a729ce0c62
Jess Robinson <castaway@desert-island.me.uk>
2011-08-03 20:36:03 +0100

    Minor insanity to ensure -lrt isn't added to libs when cross-compiling for Android

--- cpan/Time-HiRes/hints/linux.pl
+++ cpan/Time-HiRes/hints/linux.pl
@@ -1,2 +1,7 @@
 # needs to explicitly link against librt to pull in clock_nanosleep
-$self->{LIBS} = ['-lrt'];
+use Config;
+#print STDERR join ("\n", %Config);
+## This insanity is because: $^O is being used to determine which hints file is being run, I'm cross-compiling on linux, so hints/linux.pl is run. However I'm compiling for android, so osname (in config.sh-arm-linux-android) is set to android, which does not have -lrt.
+if($Config{osname} eq 'linux') {
+    $self->{LIBS} = ['-lrt'];
+}
